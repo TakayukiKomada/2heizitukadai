64ac2d42329bc7f754dde086d749ee92c5812726
TakayukiKomada <Takakoma.79@gmail.com>
2022-06-27 16:37:38 +0900

    途中経過

--- Hackersnewsapi.py
+++ Hackersnewsapi.py
@@ -1,12 +1,27 @@
 import requests
-import time
 
-
-for i in range(10):
-    time.sleep(1)
-    print(i)
-    zipcode = '160705'
-
+# import time
+zipcode = "8863"
+zipcode = "160705"
 response = requests.get(
-    "https://hacker-news.firebaseio.com/v0/item/{zipcode}.json?print=pretty"
+    f"https://hacker-news.firebaseio.com/v0/item/{zipcode}.json?print=pretty"
 )
+dic = response.json()
+response_2 = requests.get(
+    f"https://hacker-news.firebaseio.com/v0/item/{zipcode_2}.json?print=pretty"
+)
+
+print(response)
+print(response_2)
+news_title = dic['title']
+news_url = dic['url']
+print(news_title)
+print(news_url)
+
+
+
+
+
+# for i in range(10):
+#     time.sleep(1)
+#     print(i)
