8f3557e9160c9765d1493e22e6743fcb41bfd406
TakayukiKomada <Takakoma.79@gmail.com>
2022-06-27 18:02:28 +0900

    とりあえずタイトルとURLを表示させるとこまで

--- Hackersnewsapi.py
+++ Hackersnewsapi.py
@@ -1,21 +1,17 @@
 import requests
 
 # import time
-zipcode = "8863"
-zipcode_2 = "160705"
+zipcode = '8863'
 response = requests.get(
     f"https://hacker-news.firebaseio.com/v0/item/{zipcode}.json?print=pretty"
 )
 dic = response.json()
-response_2 = requests.get(
-    f"https://hacker-news.firebaseio.com/v0/item/{zipcode_2}.json?print=pretty"
-)
+
 print(response)
-print(response_2)
+
 news_title = dic['title']
 news_url = dic['url']
-print(news_title)
-print(news_url)
+print(f"'title':{news_title}'url':{news_url}")
 
 
 
